5c6405ad30d1929d6e581f28ee7468a55878db81
Sougata Roy <sougata.roy26@yahoo.com>
2025-11-23 22:45:11 +0530

    Install react navigation with Stack and Tab Navigator

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { StatusBar, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <StatusBar barStyle={'default'} />
+      <Text>Hello World!</Text>
+    </View>
+  );
+}

a94ebdc051150e50b2b5457bd96db4af08b4fd84
Sougata Roy <sougata.roy26@yahoo.com>
2025-11-26 17:35:06 +0530

    Added remaining screens

---
+++ src/screens/app/AddBudgetScreen.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function AddBudgetScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Add Budget Screen</Text>
+    </SafeAreaView>
+  );
+}

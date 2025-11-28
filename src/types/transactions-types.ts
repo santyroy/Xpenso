2e4c6172807a9211d3042393eb433d11a9e9fd98
Sougata Roy <sougata.roy26@yahoo.com>
2025-11-28 17:49:43 +0530

    Add Category type

--- src/types/transactions-types.ts
+++ src/types/transactions-types.ts
@@ -1,1 +1,9 @@
+import { FontAwesome6SolidIconName } from '@react-native-vector-icons/fontawesome6';
+
 export type TransactionType = 'income' | 'expense';
+
+export type Category = {
+  id: number;
+  name: string;
+  iconName: FontAwesome6SolidIconName;
+};

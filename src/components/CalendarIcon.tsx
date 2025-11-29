1e575c3aa62888e7144f226fb025571888349c4c
Sougata Roy <sougata.roy26@yahoo.com>
2025-11-29 10:30:11 +0530

    Add CalendarIcon component

---
+++ src/components/CalendarIcon.tsx
@@ -0,0 +1,13 @@
+import FontAwesome6 from '@react-native-vector-icons/fontawesome6';
+import { Pressable, PressableProps } from 'react-native';
+import { useAppTheme } from '../hooks/useAppTheme';
+
+export default function CalendarIcon(props: PressableProps) {
+  const { colors } = useAppTheme();
+
+  return (
+    <Pressable {...props}>
+      <FontAwesome6 name="calendar-days" size={18} color={colors.text} />
+    </Pressable>
+  );
+}

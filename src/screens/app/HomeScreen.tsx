c2493299e10f36ec85853c293d3a8646443bc4d3
Sougata Roy <sougata.roy26@yahoo.com>
2025-12-09 20:22:09 +0530

    Add new components to HomeScreen

--- src/screens/app/HomeScreen.tsx
+++ src/screens/app/HomeScreen.tsx
@@ -1,10 +1,21 @@
-import { Text } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Header from '../../components/Header';
+import SummaryCard from '../../components/SummaryCard';
+import RecentBudgets from '../../components/RecentBudgets';
+import RecentTransactions from '../../components/RecentTransactions';
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <Text>Home Screen</Text>
+    <SafeAreaView style={styles.container}>
+      <Header greetings="Good Morning" name="Surabhi" />
+      <SummaryCard income={60000} expense={30000} month="Dec 2025" />
+      <RecentBudgets />
+      <RecentTransactions />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, gap: 20, paddingHorizontal: 10 },
+});

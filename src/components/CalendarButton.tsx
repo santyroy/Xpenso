c415ef7b3fd0fcca34be5f3c0decdb9fe0503dc7
Sougata Roy <sougata.roy26@yahoo.com>
2025-12-09 11:24:15 +0530

    Update CalendarButton component with date picker methods

--- src/components/CalendarButton.tsx
+++ src/components/CalendarButton.tsx
@@ -1,12 +1,36 @@
 import FontAwesome6 from '@react-native-vector-icons/fontawesome6';
 import { Pressable, PressableProps } from 'react-native';
+import {
+  DateTimePickerAndroid,
+  DateTimePickerEvent,
+} from '@react-native-community/datetimepicker';
 import { useAppTheme } from '../hooks/useAppTheme';
 
-export default function CalendarButton(props: PressableProps) {
+type Props = {
+  setDate: (date: string) => void;
+} & PressableProps;
+
+export default function CalendarButton({ setDate, ...rest }: Props) {
   const { colors } = useAppTheme();
 
+  const onDateChange = (event: DateTimePickerEvent, selectedDate?: Date) => {
+    const currentDate = selectedDate;
+    if (currentDate) {
+      setDate(currentDate.toDateString());
+    }
+  };
+
+  const handleDatePicker = () => {
+    DateTimePickerAndroid.open({
+      value: new Date(Date.now()),
+      onChange: onDateChange,
+      mode: 'date',
+      is24Hour: true,
+    });
+  };
+
   return (
-    <Pressable {...props}>
+    <Pressable {...rest} onPress={handleDatePicker}>
       <FontAwesome6 name="calendar-days" size={20} color={colors.text} />
     </Pressable>
   );

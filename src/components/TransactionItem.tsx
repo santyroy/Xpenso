ca692c2aa187bd9ac0d00ebff9fff3f4cfa4bf7e
Sougata Roy <sougata.roy26@yahoo.com>
2025-12-10 16:34:42 +0530

    Add TransactionItem component

---
+++ src/components/TransactionItem.tsx
@@ -0,0 +1,65 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import FontAwesome6 from '@react-native-vector-icons/fontawesome6';
+import { Transaction } from '../types/transactions-types';
+import { useAppTheme } from '../hooks/useAppTheme';
+import { capitalize } from '../utils/text-utils';
+
+type Props = {
+  item: Transaction;
+};
+
+export default function TransactionItem({ item }: Props) {
+  const {
+    amount,
+    category: { name, iconName, color },
+    date,
+    type,
+    note,
+  } = item;
+  const { colors } = useAppTheme();
+
+  const amountText = type === 'expense' ? `- ${amount}` : `+ ${amount}`;
+  const amountColor = type === 'expense' ? colors.notification : colors.primary;
+  const categoryName = capitalize(name);
+  const noteText =
+    note && note?.length > 10 ? note?.substring(0, 10) + '...' : note;
+
+  return (
+    <Pressable style={[styles.container]}>
+      <View style={styles.categoryContainer}>
+        <FontAwesome6
+          name={iconName}
+          iconStyle="solid"
+          size={24}
+          color={color}
+          style={[{ backgroundColor: colors.border }, styles.icon]}
+        />
+        <View>
+          <Text style={[{ color: colors.text }, styles.categoryText]}>
+            {categoryName}
+          </Text>
+          <Text style={[{ color: colors.text }]}>{noteText}</Text>
+        </View>
+      </View>
+      <View style={styles.amountContainer}>
+        <Text style={[{ color: amountColor }, styles.amountText]}>
+          {amountText}
+        </Text>
+        <Text style={[{ color: colors.text }]}>{date.toDateString()}</Text>
+      </View>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flexDirection: 'row', justifyContent: 'space-between' },
+  categoryContainer: {
+    flexDirection: 'row',
+    gap: 10,
+    alignItems: 'center',
+  },
+  icon: { padding: 10, borderRadius: 10 },
+  categoryText: { fontSize: 18 },
+  amountContainer: { alignItems: 'flex-end' },
+  amountText: { fontSize: 18, fontWeight: 600 },
+});

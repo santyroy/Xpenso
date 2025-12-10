fe9bca6607c2dca533987c70b2184015e686d85b
Sougata Roy <sougata.roy26@yahoo.com>
2025-12-10 16:59:24 +0530

    Integrate useTrasactionHook into TransactionScreen

--- src/screens/app/TransactionsScreen.tsx
+++ src/screens/app/TransactionsScreen.tsx
@@ -1,10 +1,23 @@
-import { Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { FlatList, StyleSheet, View } from 'react-native';
+import TransactionItem from '../../components/TransactionItem';
+import { useTransactions } from '../../hooks/useTransactions';
 
 export default function TransactionsScreen() {
+  const { transactions } = useTransactions();
+
   return (
-    <SafeAreaView>
-      <Text>Transactions Screen</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <FlatList
+        data={transactions}
+        renderItem={({ item }) => <TransactionItem item={item} />}
+        keyExtractor={item => item.id}
+        contentContainerStyle={styles.contentStyle}
+      />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { padding: 20 },
+  contentStyle: { gap: 20 },
+});

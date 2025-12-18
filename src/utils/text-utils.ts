99cc9982749e69b261fc4fd82ba4b5d22972b93a
Sougata Roy <sougata.roy26@yahoo.com>
2025-12-18 09:03:35 +0530

    Add greetMessage utility function

--- src/utils/text-utils.ts
+++ src/utils/text-utils.ts
@@ -2,3 +2,11 @@ export const capitalize = (text: string) => {
   if (!text || text.length === 0) return;
   return text.substring(0, 1).toUpperCase() + text.substring(1);
 };
+
+export const greetMessage = () => {
+  const currentHours = new Date().getHours();
+  if (currentHours < 6) return 'Good Night';
+  if (currentHours < 12) return 'Good Morning';
+  if (currentHours < 16) return 'Good Afternoon';
+  return 'Good Evening';
+};

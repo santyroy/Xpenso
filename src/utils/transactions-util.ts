c94798fe5b259eced714c3b7835f121670befc25
Sougata Roy <sougata.roy26@yahoo.com>
2025-12-18 22:43:50 +0530

    Add function getTransactionSummary

---
+++ src/utils/transactions-util.ts
@@ -0,0 +1,16 @@
+import { Transaction } from '../types/transactions-types';
+
+export const getTransactionSummary = (transactions: Transaction[]) => {
+  return transactions.reduce(
+    (acc, curr) => {
+      if (curr.type === 'income') {
+        acc.income += curr.amount;
+      }
+      if (curr.type === 'expense') {
+        acc.expense += curr.amount;
+      }
+      return acc;
+    },
+    { income: 0, expense: 0 },
+  );
+};

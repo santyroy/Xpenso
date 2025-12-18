86e9fd8b02a48d9ff71717ba391d7b9a830058c4
Sougata Roy <sougata.roy26@yahoo.com>
2025-12-18 22:47:41 +0530

    Update HomeScreen to dynamically update SummaryCard and RecentTransactions components

--- src/screens/app/HomeScreen.tsx
+++ src/screens/app/HomeScreen.tsx
@@ -1,17 +1,28 @@
+import { useMemo } from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Header from '../../components/Header';
 import SummaryCard from '../../components/SummaryCard';
 import RecentTransactions from '../../components/RecentTransactions';
-import { greetMessage } from '../../utils/text-utils';
+import { useTransactions } from '../../hooks/useTransactions';
+import { getMonthYearString, greetMessage } from '../../utils/text-utils';
+import { getTransactionSummary } from '../../utils/transactions-util';
 
 export default function HomeScreen() {
-  const greetings = greetMessage();
+  const greetings = useMemo(() => greetMessage(), []);
+  const month = useMemo(() => getMonthYearString(), []);
+  const { transactions } = useTransactions({ limit: 0 });
+  const { income, expense } = useMemo(
+    () => getTransactionSummary(transactions),
+    [transactions],
+  );
+  const recentData = useMemo(() => transactions.slice(0, 5), [transactions]);
+
   return (
     <SafeAreaView style={styles.container}>
       <Header greetings={greetings} name="Surabhi" />
-      <SummaryCard income={60000} expense={30000} month="Dec 2025" />
-      <RecentTransactions />
+      <SummaryCard income={income} expense={expense} month={month} />
+      <RecentTransactions transactions={recentData} />
     </SafeAreaView>
   );
 }

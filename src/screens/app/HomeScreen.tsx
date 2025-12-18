389c7a26d70574e550ce63dad16333df600fe970
Sougata Roy <sougata.roy26@yahoo.com>
2025-12-18 09:04:05 +0530

    Integrate greetMessage in HomeScreen

--- src/screens/app/HomeScreen.tsx
+++ src/screens/app/HomeScreen.tsx
@@ -3,11 +3,13 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import Header from '../../components/Header';
 import SummaryCard from '../../components/SummaryCard';
 import RecentTransactions from '../../components/RecentTransactions';
+import { greetMessage } from '../../utils/text-utils';
 
 export default function HomeScreen() {
+  const greetings = greetMessage();
   return (
     <SafeAreaView style={styles.container}>
-      <Header greetings="Good Morning" name="Surabhi" />
+      <Header greetings={greetings} name="Surabhi" />
       <SummaryCard income={60000} expense={30000} month="Dec 2025" />
       <RecentTransactions />
     </SafeAreaView>

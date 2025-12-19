8bc48d6198c4b9e21bc1d33259aed4ff5dca9ec4
Sougata Roy <sougata.roy26@yahoo.com>
2025-12-19 11:54:27 +0530

    Add budget-types

---
+++ src/types/budget-types.ts
@@ -0,0 +1,37 @@
+import { FontAwesome6SolidIconName } from '@react-native-vector-icons/fontawesome6';
+
+export type BudgetType = 'expense';
+
+export type Category = {
+  id: number;
+  name: string;
+  iconName: FontAwesome6SolidIconName;
+  color: string;
+};
+
+export type Budget = {
+  id: string;
+  type: BudgetType;
+  amountLimit: number;
+  category: Category;
+  startDate: Date;
+  endDate: Date;
+};
+
+export type BudgetForm = {
+  type: BudgetType;
+  amountLimit: string;
+  category: Category | undefined;
+  startDate: string;
+  period: string;
+};
+
+// Used for API, Storage, and NAVIGATION PARAMS
+export type SerializableBudget = {
+  id: string;
+  type: BudgetType;
+  amount: string;
+  category: Category;
+  startDate: string; // ISO date string
+  period: string;
+};

c10bb012ce777c5d8a94387713fbab212c51519c
Sougata Roy <sougata.roy26@yahoo.com>
2025-12-19 16:26:06 +0530

    Create AddBudgetScreenNavigationProp

--- src/types/navigation-types.ts
+++ src/types/navigation-types.ts
@@ -62,3 +62,8 @@ export type BudgetCompositeNavigationProp = CompositeNavigationProp<
   BudgetTabNavigationProp,
   AppNavigationProp
 >;
+
+export type AddBudgetScreenNavigationProp = NativeStackNavigationProp<
+  AppParamList,
+  'AddBudget'
+>;

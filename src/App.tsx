9892a44ac7e9a36b11e8bde58f6b11ffca09f8bf
Sougata Roy <sougata.roy26@yahoo.com>
2025-12-21 00:16:10 +0530

    Wrap entire application inside UserProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './navigations/RootNavigator';
 import { CustomTheme } from './utils/theme';
+import { UserProvider } from './contexts/UserContext';
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={'default'} backgroundColor={'transparent'} />
-      <NavigationContainer theme={CustomTheme}>
-        <RootNavigator />
-      </NavigationContainer>
+      <UserProvider>
+        <NavigationContainer theme={CustomTheme}>
+          <RootNavigator />
+        </NavigationContainer>
+      </UserProvider>
     </SafeAreaProvider>
   );
 }

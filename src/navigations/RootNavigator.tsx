b94139c1d6157c5a6a18fb3616b0ffd4a69a6b1b
Sougata Roy <sougata.roy26@yahoo.com>
2025-12-21 00:10:56 +0530

    Call useUser hook to know if user hasOnboarded

--- src/navigations/RootNavigator.tsx
+++ src/navigations/RootNavigator.tsx
@@ -2,16 +2,16 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RootParamList } from '../types/navigation-types';
 import AppNavigator from './AppNavigator';
 import AuthNavigator from './AuthNavigator';
+import { useUser } from '../hooks/useUser';
 
 const Stack = createNativeStackNavigator<RootParamList>();
 
 export default function RootNavigator() {
-  // TODO: pending auth logic
-  const isAuthenticated = true;
+  const { hasOnboarded } = useUser();
 
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      {isAuthenticated ? (
+    <Stack.Navigator screenOptions={{ headerShown: false, animation: 'fade' }}>
+      {hasOnboarded ? (
         <Stack.Screen name="App" component={AppNavigator} />
       ) : (
         <Stack.Screen name="Auth" component={AuthNavigator} />

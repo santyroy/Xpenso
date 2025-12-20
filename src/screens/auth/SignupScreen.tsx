67e9a02bf09a18a7bcbd933c919c5229ec5884c9
Sougata Roy <sougata.roy26@yahoo.com>
2025-12-21 00:10:07 +0530

    Integrate SignupHeader and SignupForm in SignupScreen

--- src/screens/auth/SignupScreen.tsx
+++ src/screens/auth/SignupScreen.tsx
@@ -1,10 +1,26 @@
-import { Text } from 'react-native';
+import { KeyboardAvoidingView, Platform, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import SignupForm from '../../components/SignupForm';
+import SignupHeader from '../../components/SignupHeader';
 
 export default function SignupScreen() {
   return (
-    <SafeAreaView>
-      <Text>Signup Screen</Text>
+    <SafeAreaView style={styles.container}>
+      <SignupHeader pageTitle="Signup!" />
+      <KeyboardAvoidingView
+        style={styles.formContainer}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      >
+        <SignupForm />
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  formContainer: { flex: 1, justifyContent: 'center' },
+});

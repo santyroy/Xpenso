d7ce262f4ebf18343955a41851f190dd6f486a2f
Sougata Roy <sougata.roy26@yahoo.com>
2025-12-21 01:31:24 +0530

    Integrate react native bootsplash in RootNavigator

--- src/navigations/RootNavigator.tsx
+++ src/navigations/RootNavigator.tsx
@@ -1,5 +1,7 @@
+import { useEffect } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RootParamList } from '../types/navigation-types';
+import BootSplash from 'react-native-bootsplash';
 import AppNavigator from './AppNavigator';
 import AuthNavigator from './AuthNavigator';
 import { useUser } from '../hooks/useUser';
@@ -7,7 +9,23 @@ import { useUser } from '../hooks/useUser';
 const Stack = createNativeStackNavigator<RootParamList>();
 
 export default function RootNavigator() {
-  const { hasOnboarded } = useUser();
+  const { hasOnboarded, isLoading } = useUser();
+
+  useEffect(() => {
+    const hideSplash = async () => {
+      if (!isLoading) {
+        await BootSplash.hide({ fade: true });
+      }
+    };
+
+    hideSplash();
+  }, [isLoading]);
+
+  if (isLoading) {
+    // Keep returning null while loading so the
+    // Native Splash remains visible and undisturbed.
+    return null;
+  }
 
   return (
     <Stack.Navigator screenOptions={{ headerShown: false, animation: 'fade' }}>

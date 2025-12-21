3e895285fdd1b4a22be651f67979411a5539d5fa
Sougata Roy <sougata.roy26@yahoo.com>
2025-12-22 00:04:01 +0530

    Implement Charts in AnalyticsScreen

--- src/screens/app/AnalyticsScreen.tsx
+++ src/screens/app/AnalyticsScreen.tsx
@@ -1,10 +1,30 @@
-import { Text } from 'react-native';
+import { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { TransactionType } from '../../types/transaction-types';
+import TransactionTypeToggleButtons from '../../components/TransactionTypeToogleButtons';
+import { StyleSheet } from 'react-native';
+import ExpenseAnalytics from '../../components/ExpenseAnalytics';
+import IncomeAnalytics from '../../components/IncomeAnalytics';
 
 export default function AnalyticsScreen() {
+  const [transactionType, setTransactionType] =
+    useState<TransactionType>('expense');
+
   return (
-    <SafeAreaView>
-      <Text>Analytics Screen</Text>
+    <SafeAreaView style={styles.container}>
+      <TransactionTypeToggleButtons
+        transactionType={transactionType}
+        setTransactionType={setTransactionType}
+      />
+      {transactionType === 'expense' ? (
+        <ExpenseAnalytics />
+      ) : (
+        <IncomeAnalytics />
+      )}
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 20, gap: 20 },
+});

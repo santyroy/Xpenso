ebf859f19f5fca77e70d3f29d1bea04adb018157
Sougata Roy <sougata.roy26@yahoo.com>
2025-12-22 20:50:54 +0530

    Implement profile pic update in ProfileScreen

--- src/screens/app/ProfileScreen.tsx
+++ src/screens/app/ProfileScreen.tsx
@@ -2,6 +2,7 @@ import { useCallback, useState } from 'react';
 import {
   Image,
   Platform,
+  Pressable,
   StyleSheet,
   Text,
   ToastAndroid,
@@ -9,6 +10,9 @@ import {
 } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { launchImageLibrary } from 'react-native-image-picker';
+import { Dirs } from 'react-native-file-access';
+import FontAwesome6 from '@react-native-vector-icons/fontawesome6';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import Loading from '../../components/Loading';
@@ -19,14 +23,25 @@ import { useUser } from '../../hooks/useUser';
 import { useAppTheme } from '../../hooks/useAppTheme';
 import { SignupFormError } from '../../types/errors-types';
 import { countryName } from '../../utils/text-utils';
+import { DEFAULT_AVATAR } from '../../utils/file-utils';
 
 export default function ProfileScreen() {
-  const { isLoading, name, updateName, country, updateCountry } = useUser();
+  const {
+    isLoading,
+    name,
+    updateName,
+    country,
+    updateCountry,
+    profilePicFilename,
+    updateProfilePic,
+  } = useUser();
   const [userName, setUserName] = useState('');
   const [userCountry, setUserCountry] = useState(country);
   const [errors, setErrors] = useState<SignupFormError>({});
   const { colors } = useAppTheme();
-  const userAvatar = require('../../../assets/images/user-avatar.webp');
+  const userAvatar = profilePicFilename
+    ? { uri: `file://${Dirs.DocumentDir}/${profilePicFilename}` }
+    : DEFAULT_AVATAR;
 
   // Keep the local input in sync with the Context data
   useFocusEffect(
@@ -48,7 +63,7 @@ export default function ProfileScreen() {
       err.name = 'Name is required';
     }
     if (userCountry.trim() === '') {
-      err.name = 'Country is required';
+      err.country = 'Country is required';
     }
 
     setErrors(err);
@@ -62,6 +77,18 @@ export default function ProfileScreen() {
     }
   };
 
+  const handleImagePicker = async () => {
+    const result = await launchImageLibrary({ mediaType: 'photo' });
+    if (result.didCancel) return;
+
+    if (result.assets?.length) {
+      const tempUri = result.assets[0].uri;
+      if (tempUri) {
+        updateProfilePic(tempUri);
+      }
+    }
+  };
+
   if (isLoading) {
     return (
       <View style={styles.loadingContainer}>
@@ -72,8 +99,19 @@ export default function ProfileScreen() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.profileContainer}>
+      <View style={[styles.profileContainer, { borderColor: colors.border }]}>
         <Image source={userAvatar} style={styles.profilePicture} />
+        <Pressable
+          onPress={handleImagePicker}
+          style={[styles.imagePicker, { backgroundColor: colors.text }]}
+        >
+          <FontAwesome6
+            name="image"
+            iconStyle="solid"
+            size={18}
+            color={colors.background}
+          />
+        </Pressable>
       </View>
       <Text style={[styles.userInfoText, { color: colors.text }]}>
         User Information
@@ -106,8 +144,15 @@ export default function ProfileScreen() {
 const styles = StyleSheet.create({
   container: { flex: 1, paddingHorizontal: 20, gap: 20 },
   loadingContainer: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-  profileContainer: { alignItems: 'center' },
+  profileContainer: { alignItems: 'center', marginHorizontal: 'auto' },
   profilePicture: { height: 100, aspectRatio: 1, borderRadius: 50 },
+  imagePicker: {
+    position: 'absolute',
+    bottom: 4,
+    right: 0,
+    padding: 6,
+    borderRadius: 15,
+  },
   userInfoCard: { borderRadius: 14, padding: 20, gap: 20 },
   userInfoText: { fontSize: 18, fontWeight: 600, textAlign: 'center' },
   formGroup: { gap: 5 },

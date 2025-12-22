6eb1dcd3abd0112e169074240af24c3732512d0b
Sougata Roy <sougata.roy26@yahoo.com>
2025-12-22 09:28:04 +0530

    Update ProfileScreen to manage country

--- src/screens/app/ProfileScreen.tsx
+++ src/screens/app/ProfileScreen.tsx
@@ -13,13 +13,17 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 import Loading from '../../components/Loading';
 import Error from '../../components/Error';
+import Label from '../../components/Label';
+import DropDown from '../../components/DropDown';
 import { useUser } from '../../hooks/useUser';
 import { useAppTheme } from '../../hooks/useAppTheme';
 import { SignupFormError } from '../../types/errors-types';
+import { countryName } from '../../utils/text-utils';
 
 export default function ProfileScreen() {
-  const { isLoading, name, updateName } = useUser();
+  const { isLoading, name, updateName, country, updateCountry } = useUser();
   const [userName, setUserName] = useState('');
+  const [userCountry, setUserCountry] = useState(country);
   const [errors, setErrors] = useState<SignupFormError>({});
   const { colors } = useAppTheme();
 
@@ -29,8 +33,11 @@ export default function ProfileScreen() {
       if (name) {
         setUserName(name);
       }
+      if (country) {
+        setUserCountry(country);
+      }
       setErrors({});
-    }, [name]),
+    }, [name, country]),
   );
 
   const handleUpdateProfile = () => {
@@ -39,11 +46,15 @@ export default function ProfileScreen() {
     if (userName.trim() === '') {
       err.name = 'Name is required';
     }
+    if (userCountry.trim() === '') {
+      err.name = 'Country is required';
+    }
 
     setErrors(err);
     if (Object.values(err).length !== 0) return;
 
     updateName(userName);
+    updateCountry(userCountry.trim());
 
     if (Platform.OS === 'android') {
       ToastAndroid.show('Profile Update successful', ToastAndroid.SHORT);
@@ -68,14 +79,30 @@ export default function ProfileScreen() {
           style={styles.profilePicture}
         />
       </View>
+      <Text style={[styles.userInfoText, { color: colors.text }]}>
+        User Information
+      </Text>
       <View style={[styles.userInfoCard, { backgroundColor: colors.card }]}>
-        <Text style={[styles.userInfoText, { color: colors.text }]}>
-          User Information
-        </Text>
-        <Input state={userName} setState={setUserName} />
-        {errors.name && <Error errorMsg={errors.name} />}
+        <View style={styles.formGroup}>
+          <Label text="Name" />
+          <Input state={userName} setState={setUserName} />
+          {errors.name && <Error errorMsg={errors.name} />}
+        </View>
+        <View style={styles.formGroup}>
+          <Label text="Country" />
+          <View
+            style={[styles.countryDropDown, { borderColor: colors.border }]}
+          >
+            <DropDown
+              list={countryName}
+              state={userCountry}
+              setState={setUserCountry}
+            />
+          </View>
+          {errors.country && <Error errorMsg={errors.country} />}
+        </View>
+        <Button text="Update Profile" onPress={handleUpdateProfile} />
       </View>
-      <Button text="Update Profile" onPress={handleUpdateProfile} />
     </SafeAreaView>
   );
 }
@@ -85,6 +112,8 @@ const styles = StyleSheet.create({
   loadingContainer: { flex: 1, justifyContent: 'center', alignItems: 'center' },
   profileContainer: { alignItems: 'center' },
   profilePicture: { height: 100, aspectRatio: 1, borderRadius: 50 },
-  userInfoCard: { borderRadius: 14, padding: 20, gap: 10 },
-  userInfoText: { fontSize: 18, fontWeight: 600 },
+  userInfoCard: { borderRadius: 14, padding: 20, gap: 20 },
+  userInfoText: { fontSize: 18, fontWeight: 600, textAlign: 'center' },
+  formGroup: { gap: 5 },
+  countryDropDown: { borderWidth: 2, borderRadius: 14, overflow: 'hidden' },
 });

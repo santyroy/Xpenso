8dcbad74e4eebf3bb40fa1dfb8c1a1eed76e7185
Sougata Roy <sougata.roy26@yahoo.com>
2026-01-01 23:10:00 +0530

    Add Year DropDown in TransactionsScreen

--- src/screens/app/TransactionsScreen.tsx
+++ src/screens/app/TransactionsScreen.tsx
@@ -4,12 +4,13 @@ import { useTransactions } from '../../hooks/useTransactions';
 import { useAppTheme } from '../../hooks/useAppTheme';
 import TransactionList from '../../components/TransactionList';
 import DropDown from '../../components/DropDown';
-import { months } from '../../utils/text-utils';
+import { generatePreviousYears, months } from '../../utils/text-utils';
 
 export default function TransactionsScreen() {
   const [selectedMonth, setSelectedMonth] = useState(
     months[new Date().getMonth()],
   );
+  const [year, setYear] = useState(String(new Date().getFullYear()));
   const { transactions } = useTransactions({
     limit: 0,
     month: months.indexOf(selectedMonth),
@@ -18,18 +19,28 @@ export default function TransactionsScreen() {
 
   return (
     <View style={styles.container}>
-      <View
-        style={[
-          styles.dropdownContainer,
-          { backgroundColor: colors.secondary },
-        ]}
-      >
-        <DropDown
-          list={months}
-          state={selectedMonth}
-          setState={setSelectedMonth}
-        />
+      <View style={[styles.dropdownContainer]}>
+        <View
+          style={[styles.dateContainer, { backgroundColor: colors.secondary }]}
+        >
+          <DropDown
+            list={months}
+            state={selectedMonth}
+            setState={setSelectedMonth}
+          />
+        </View>
+
+        <View
+          style={[styles.dateContainer, { backgroundColor: colors.secondary }]}
+        >
+          <DropDown
+            list={generatePreviousYears()}
+            state={year}
+            setState={setYear}
+          />
+        </View>
       </View>
+
       <View style={styles.listStyle}>
         <TransactionList transactions={transactions} />
       </View>
@@ -40,11 +51,15 @@ export default function TransactionsScreen() {
 const styles = StyleSheet.create({
   container: { flex: 1, paddingHorizontal: 15, paddingTop: 20 },
   dropdownContainer: {
-    width: '60%',
-    marginHorizontal: 'auto',
+    flexDirection: 'row',
+    gap: 10,
     marginBottom: 20,
-    paddingHorizontal: 10,
-    borderRadius: 10,
+  },
+  dateContainer: {
+    flex: 1,
+    paddingLeft: 10,
+    overflow: 'hidden',
+    borderRadius: 12,
   },
   listStyle: { flex: 1 },
 });
